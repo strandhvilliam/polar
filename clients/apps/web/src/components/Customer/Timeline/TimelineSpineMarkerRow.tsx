706103462c3b7c1369c98ce3cffb604a3008ff0d
Villiam Strandh <villiam.strandh@outlook.com>
2026-04-20 07:42:30 +0200

    fix(timeline): update TimelineSpineMarkerRow styling to have circles be filled

--- clients/apps/web/src/components/Customer/Timeline/TimelineSpineMarkerRow.tsx
+++ clients/apps/web/src/components/Customer/Timeline/TimelineSpineMarkerRow.tsx
@@ -12,7 +12,7 @@ export const TimelineSpineMarkerRow = ({
 }) => (
   <div className="relative flex items-center pt-2 pb-4">
     <div className="relative z-10 flex w-6 shrink-0 justify-center">
-      <div className="size-3 shrink-0 rounded-full border-2 border-blue-500 bg-white dark:bg-transparent" />
+      <div className="dark:bg-polar-950 size-3 shrink-0 rounded-full border-2 border-blue-500 bg-white" />
     </div>
     <div className="dark:bg-polar-700 -ml-1.5 h-0.5 w-3 shrink-0 bg-gray-200" />
     <div className={twMerge('flex min-w-0 flex-1', contentClassName)}>
